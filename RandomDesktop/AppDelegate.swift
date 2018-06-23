91b5081d4949f05d1277105c21c1e2b5cad398f5
its-me2000 <Eugenijus@eugenijuss-imac.local>
2018-06-23 17:45:53 +0200

    Initial Commit

---
+++ RandomDesktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomDesktop
+//
+//  Created by Eugenijus Margalikas on 23/06/2018.
+//  Copyright © 2018 Eugenijus Margalikas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomDesktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomDesktop
+//
+//  Created by Eugenijus Margalikas on 23/06/2018.
+//  Copyright © 2018 Eugenijus Margalikas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
